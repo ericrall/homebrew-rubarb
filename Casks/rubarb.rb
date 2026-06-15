6f36b5b9da22e2801b347e276d4034c7f0bf4443
Eric Rall <rall.eric@gmail.com>
2026-06-15 17:05:03 -0400

    rubarb 2.4.13

--- Casks/rubarb.rb
+++ Casks/rubarb.rb
@@ -1,6 +1,6 @@
 cask "rubarb" do
-  version "2.4.12"
-  sha256 "800ffe9d8bdd129e90fe63eddd15ec0ac5ee19e65e5ddf3b5d216016f8d3bdc6"
+  version "2.4.13"
+  sha256 "799a73cdecde9cfdfeda4c069129d02e068f3cc016967344d0ecb6cc0bc55f28"
 
   url "https://rubarb.bar/rubarb.bar-#{version}.zip"
   name "Rubarb"
